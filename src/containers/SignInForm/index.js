ec336f38bdf32bc2f0cc865ec42b216b4d2126a2
Bohdan Khodakivskyi <bohdan.k@fetcher.us>
2017-12-17 00:53:22 +0400

    basic login

--- src/containers/SignInForm/index.js
+++ src/containers/SignInForm/index.js
@@ -1,15 +1,63 @@
-// @flow
-
 import React from 'react';
-import { View, TextInput, Button } from 'react-native';
+import { View, TextInput, Button, Text } from 'react-native';
+import { connect } from 'react-redux';
+import firebase from 'react-native-firebase';
+import { userSignIn } from '@modules/user';
 import styles from './styles';
 
-export const SignInForm = () => {
-  return (
-    <View>
-      <TextInput placeholder="Email" keyboardType="email-address" />
-      <TextInput placeholder="Password" secureTextEntry={true} />
-      <Button onPress={() => false} title="Sign In" />
-    </View>
-  );
-};
+class SignInForm extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { email: '', password: '' };
+  }
+
+  onSubmit() {
+    const { email, password } = this.state;
+    console.log(email, password);
+    const signIn = firebase.auth().signInWithEmailAndPassword(email, password);
+    signIn
+      .then(data => {
+        console.log(data);
+        this.props.userSignIn(data._user);
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  render() {
+    const { containerStyle, inputStyle, buttonStyle } = styles;
+
+    return (
+      <View style={containerStyle}>
+        <TextInput
+          style={inputStyle}
+          placeholder="EMAIL"
+          keyboardType="email-address"
+          autoCorrect={false}
+          returnKeyType="next"
+          value={this.state.email}
+          onChangeText={email => this.setState({ email })}
+        />
+        <TextInput
+          style={inputStyle}
+          placeholder="PASSWORD"
+          secureTextEntry={true}
+          onChangeText={password => this.setState({ password })}
+        />
+        <Button
+          color="#FFFFFF"
+          onPress={this.onSubmit.bind(this)}
+          title="SIGN IN"
+        />
+      </View>
+    );
+  }
+}
+
+const mapDispatchToProps = dispatch => ({
+  userSignIn: payload => dispatch(userSignIn(payload)),
+});
+
+export default connect(null, mapDispatchToProps)(SignInForm);
--- src/containers/SignInForm/styles.js
+++ src/containers/SignInForm/styles.js
@@ -1,4 +1,19 @@
-const styles = {};
+const styles = {
+  containerStyle: {
+    padding: 30,
+    width: '100%',
+  },
+  inputStyle: {
+    backgroundColor: '#fff',
+    fontSize: 16,
+    textAlign: 'center',
+    marginBottom: 15,
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    padding: 15,
+    borderRadius: 3,
+    width: '80%',
+  },
+};
 
 export default styles;
-
