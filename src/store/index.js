f40718405ad537a51f19006f1fa375a631a62a0d
byzyk <bohdan.kh@gmail.com>
2017-12-18 14:48:56 +0400

    minor flow fixes, console issue

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,19 @@ import * as reducers from '@store/reducers';
 import { logger } from '@store/middlewares/logger';
 import { auth } from '@store/middlewares/auth';
 
+const isDebug = typeof atob !== 'undefined';
+
+// FIXME: refactor this
+// just to make app running without debugger enabled
+if (!isDebug) {
+  // $FlowExpectedError
+  console.log = function() {};
+  // $FlowExpectedError
+  console.groupCollapsed = function() {};
+  // $FlowExpectedError
+  console.groupEnd = function() {};
+}
+
 const store = createStore(
   combineReducers({ ...reducers }),
   applyMiddleware(logger, auth),
