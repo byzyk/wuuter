f97af78cc63475d3c3ba9d57d64b233c8807addf
byzyk <bohdan.kh@gmail.com>
2017-12-19 13:11:46 +0400

    add icons

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -3,10 +3,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import Home from '@containers/Home';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export class HomeScreen extends React.Component<{}> {
   static navigationOptions = {
     title: 'Home',
+    tabBarIcon: () => <Icon name="home" size={24} color="#4472CA" />,
   };
 
   render() {
--- src/screens/Profile.js
+++ src/screens/Profile.js
@@ -3,10 +3,12 @@
 import React from 'react';
 import { View } from 'react-native';
 import Profile from '@containers/Profile';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export class ProfileScreen extends React.Component<{}> {
   static navigationOptions = {
     title: 'Profile',
+    tabBarIcon: () => <Icon name="user" size={24} color="#4472CA" />,
   };
 
   render() {
