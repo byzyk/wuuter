9a0b94207cef11106fc94b5d2ad8151c4708f129
byzyk <bohdan.kh@gmail.com>
2017-12-22 17:26:27 +0400

    clean up

--- src/store/modules/user/index.js
+++ src/store/modules/user/index.js
@@ -1,17 +1,30 @@
-export const USER_SIGNIN = 'app/user/SIGNIN';
-export const USER_SIGNOUT = 'app/user/SIGNOUT';
+export const SIGNIN_REQUEST = 'app/user/SIGNIN_REQUEST';
+export const SIGNIN_SUCCESS = 'app/user/SIGNIN_SUCCESS';
+export const SIGNIN_FAILURE = 'app/user/SIGNIN_FAILURE';
+export const SIGNOUT = 'app/user/SIGNOUT';
 
 const initialState = {};
 
 export default (state: any = initialState, action: any = {}) => {
   switch (action.type) {
-    case USER_SIGNIN:
+    case SIGNIN_SUCCESS:
       return { ...action.payload };
-    case USER_SIGNOUT:
+    case SIGNOUT:
       return {};
     default:
       return state;
   }
 };
 
-export { userSignIn, userSignOut } from './actions';
+export function signIn(payload) {
+  return {
+    type: SIGNIN_SUCCESS,
+    payload,
+  };
+}
+
+export function signOut() {
+  return {
+    type: SIGNOUT,
+  };
+}
